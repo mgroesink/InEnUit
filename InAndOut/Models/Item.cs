afb15e5062dfd9ee14f7278684faf77e0d423aec
Marcel Roesink <RSK01@rocvantwente.nl>
2021-09-10 11:15:12 +0200

    Model Item updated

--- InAndOut/Models/Item.cs
+++ InAndOut/Models/Item.cs
@@ -8,35 +8,12 @@ namespace InAndOut.Models
 {
     public class Item
     {
-        //// Private field
-        //private int _id;
-        //private string _borrower;
-
-        //// Property
-        //public int Id
-        //{
-        //    get { return _id; }
-        //    set
-        //    {
-
-        //            _id = Math.Abs(value);
-
-        //    }
-        //}
-
-        //public string Borrower
-        //{
-        //    get { return _borrower; }
-        //    set
-        //    {
-        //        _borrower = value.ToUpper();
-
-        //    }
-        //}
 
         [Key]
         public int Id { get; set; }
         public string Borrower { get; set; }
+        public string Lender { get; set; }
+        public string ItemName { get; set; }
 
     }
 }
